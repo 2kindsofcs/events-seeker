e9b0f53e5ada293f686f8b55342e879728233d3c
2kindsofcs <2kindsofcs@gmail.com>
2019-10-14 09:26:24 +0900

    Add body-parser

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const fetch = require('node-fetch');
-// server
-const parser = require('html-dom-parser');
+
 const express = require('express');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use('/client', express.static(__dirname + '/client'));
+
 
 app.get('/', function(req, res) {
   res.redirect('/client/main.html');
 });
+
 
 app.post('/addKeywords', function(req, res) {
   res.end();
