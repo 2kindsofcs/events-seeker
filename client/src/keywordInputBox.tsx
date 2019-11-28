c8c2a4ed15ef738c8654003fd5a91cba12132bb1
2kindsofcs <2kindsofcs@gmail.com>
2019-11-28 13:10:09 +0900

    Keep add keyword button with input tag close

--- client/src/keywordInputBox.tsx
+++ client/src/keywordInputBox.tsx
@@ -11,19 +11,18 @@ export default class KeywordInputBox extends React.Component<keywordInputBoxProp
 
     public render() {
         return (
-            <div className="columns">
-                <div className="column">
-                    <input value={this.props.keyword} onChange={(e) => this.props.keywordChangeHandler(e)} className="input is-primary is-normal" type="text" placeholder="여기에 키워드를 입력하세요" />
+            <div className="field has-addons">
+                <div className="control">
+                <input value={this.props.keyword} onChange={(e) => this.props.keywordChangeHandler(e)} className="input is-primary is-normal" type="text" placeholder="키워드를 입력하세요" />
                 </div>
-                <div className="column">
-                    <button className="button is-primary" onClick={() => this.props.updateEventData()}>
-                        <span className="icon is-small">
-                            <i className="fas fa-check"></i>
-                        </span>
-                        <span>추가하기</span>
-                    </button>
+                <div className="control">
+                <button className="button is-primary" onClick={() => this.props.updateEventData()}>
+                    <span className="icon is-small">
+                        <i className="fas fa-check"></i>
+                    </span>
+                    <span>추가하기</span>
+                </button>
                 </div>
-                {this.props.showKeywordWarning()}
             </div>
         );
     }
